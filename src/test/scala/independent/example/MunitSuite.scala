a030740121483f92353894841c8ed2f50693cdfb
Ehsan Mozaffari <ehsan.mozaffari@yahoo.com>
2023-10-16 03:09:56 +0300

    fix MunitSuite test

--- src/test/scala/independent/example/MunitSuite.scala
+++ src/test/scala/independent/example/MunitSuite.scala
@@ -3,7 +3,7 @@ import munit.*
 class MunitSuite extends FunSuite {
     test("hello I am the MunitSuite test example"){
         val obtained = 42
-        val expecteds = 42
+        val expected = 42
         assertEquals(expected, obtained)
     }
 }
